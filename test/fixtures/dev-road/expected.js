a18312c9ed8fa37302f066b2621a2a01c8a37dc6
mallow <mallow@MacBook-Pro-6.local>
2018-11-15 18:33:26 +0800

    init

---
+++ test/fixtures/dev-road/expected.js
@@ -0,0 +1,9 @@
+"use strict";
+
+require("/Users/mallow/Code/ke/npm/callow/lib/message/style/css");
+
+var _message2 = _interopRequireDefault(require("/Users/mallow/Code/ke/npm/callow/lib/message"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+(0, _message2.default)('xxx');
